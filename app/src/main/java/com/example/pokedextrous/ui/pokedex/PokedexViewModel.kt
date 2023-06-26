aa471f7040a0494be78f643358e9de5d3676a3e5
Nathan Kroizer <kroizer21@gmail.com>
2023-06-26 14:53:00 +0300

    first commit

---
+++ app/src/main/java/com/example/pokedextrous/ui/pokedex/PokedexViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.pokedextrous.ui.pokedex
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PokedexViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is pokedex Fragment"
+    }
+    val text: LiveData<String> = _text
+}
